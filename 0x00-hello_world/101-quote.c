30fe33aa3b92f59118b8ec4dd8f5785747b99b61
ja-abakah <ja.abakah@gmail.com>
2023-07-13 14:23:04 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific message to the standard error.
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+const char *m = "and that piece of art is useful\- Dora Korpar, 2015-10-19\n";
+write(2, m, 59);
+return (1);
+}
