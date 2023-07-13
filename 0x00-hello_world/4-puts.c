b9b01bce836e8dbcf582f5c3835ce76cc730a03c
ja-abakah <ja.abakah@gmail.com>
2023-07-13 10:13:52 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main: (A code that uses the puts function)
+ * Return: 0 (to show i wrote the right code)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
