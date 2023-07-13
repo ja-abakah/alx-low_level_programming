23fbe1571f1b84d3e05b7fcd9f02ae94e7bf0be8
ja-abakah <ja.abakah@gmail.com>
2023-07-13 13:06:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * Main: (A code that uses the puts function)
- * Return: 0 (to show i wrote the right code)
+ * main - Entry point of the program
+ *
+ * Description: A program that uses the puts function to print a message.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar
+ * and declares the outcome as a piece of art.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
