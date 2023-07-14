a8ea97d0c9406a3aeebfdca706f2c8258ee91f19
ja-abakah <ja.abakah@gmail.com>
2023-07-14 15:22:58 -0700

    ./101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,17 @@ int main(void)
 {
 int d1, d2, d3;
 
-for (d1 = 0; d1 < 9; d1++)
+for (d1 = 0; d1 < 8; d1++)
 {
-for (d2 = d1 + 1; d2 <= 9; d2++)
+for (d2 = d1 + 1; d2 < 9; d2++)
 {
 for (d3 = d2 + 1; d3 <= 9; d3++)
 {
 putchar(d1 + '0');
 putchar(d2 + '0');
+putchar(d3 + '0');
 
-if (d1 != 7 || d2 != 8 || d3 != 9)
+if (d1 != 8 || d2 != 9 || d3 != 10)
 {
 putchar(',');
 putchar(' ');
