62cae158bb4a21a0d0740fad0310692c3d9137cf
ja-abakah <ja.abakah@gmail.com>
2023-07-27 16:37:16 -0700

    pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,6 +14,7 @@
 void print_buffer(char *b, int size)
 {
 int o, j, i;
+
 o = 0;
 
 if (size <= 0)
@@ -39,10 +40,11 @@ printf(" ");
 for (i = 0; i < j; i++)
 {
 int c = *(b + o + i);
+
 if (c < 32 || c > 132)
 {
 c = '.';
-
+}
 printf("%c", c);
 }
 printf("\n");
