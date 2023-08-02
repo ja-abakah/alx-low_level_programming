2abe9a06c890a0aff651afcd4b41012f9263d228
ja-abakah <ja.abakah@gmail.com>
2023-08-02 16:18:45 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,49 +1,47 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to get the length of.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: The length of the string.
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
-else
 return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * comparator - Helper function to check if the string is a palindrome.
- * @s: The string to check.
- * @start: The starting index of the substring to check.
- * @end: The ending index of the substring to check.
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
  *
- * Return: 1 otherwise return 0.
+ * Return: 1 if palindrome, 0 if not
  */
-int comparator(char *s, int start, int end)
+int check_pal(char *s, int i, int len)
 {
-if (*(s + start) == *(s + end))
-{
-if (start == end || start == end + 1)
-return (1);
-
-return (0 + comparator(s, start + 1, end - 1));
+if (*(s + i) != *(s + len - 1))
 return (0);
-}
-
-/**
- * is_palindrome - Entry point of the program
- * Description: Checks if a string is a palindrome.
- * @s: The string to check.
- *
- * Return: 1 if @s is a palindrome, otherwise return 0.
- */
-int is_palindrome(char *s)
-{
-if (*s == '\0')
+if (i >= len)
 return (1);
-
-return (comparator(s, 0, _strlen_recursion(s) - 1));
+return (check_pal(s, i + 1, len - 1));
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,33 @@
 #include "main.h"
 
-/**
- * find_sqrt - Helper function to find the square root recursively.
- * @n: The number for which to find the square root.
- * @start: The starting value for the search.
- * @end: The ending value for the search.
- *
- * Return: The natural square root of @n, or -1 if not found.
- */
-int find_sqrt(int n, int start, int end)
-{
-int mid = (start + end) / 2;
-
-if (start <= end)
-{
-if (mid * mid == n)
-return (mid);
-else if (mid * mid < n)
-return (find_sqrt(n, mid + 1, end));
-else
-return (find_sqrt(n, start, mid - 1));
-}
-
-return (-1);
-}
+int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - Entry point of the program
- * Description: Returns the natural square root of a number.
- * @n: The number for which to find the square root.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: The natural square root of @n,
- * or -1 if @n does not have a natural square root.
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
+return (actual_sqrt_recursion(n, 0));
+}
 
-if (n == 0 || n == 1)
-return (n);
-
-return (find_sqrt(n, 1, n));
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
