70cea3c933581b919d2e2e0f7aebbb85bc40b94b
ja-abakah <ja.abakah@gmail.com>
2023-08-14 06:13:25 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure defines a type 'struct dog'
+ *              with three elements: name (a string), age (a float),
+ *              and owner (a string) to hold information about a dog.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
