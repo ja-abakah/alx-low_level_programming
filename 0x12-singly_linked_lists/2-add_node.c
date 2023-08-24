969bd67c93214bda46bf05ddbce53e2504fc76ae
ja-abakah <ja.abakah@gmail.com>
2023-08-24 15:56:24 -0700

    ingly

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,20 +12,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *n;
-unsigned int len = 0;
+        list_t *new_node;
 
-while (str[len])
-len++;
+        if (str == NULL)
+                return (NULL);
 
-n = malloc(sizeof(list_t));
-if (!n)
-return (NULL);
+        new_node = malloc(sizeof(list_t));
+        if (new_node == NULL)
+                return (NULL);
 
-n->str = strdup(str);
-n->len = len;
-n->next = (*head);
-(*head) = n;
+        new_node->str = strdup(str);
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->len = strlen(str);
+        new_node->next = *head;
+        *head = new_node;
 
-return (*head);
+        return (new_node);
 }
